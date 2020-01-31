f8693d9f0b977584260e3f705d0e040b525f0e0a
Jenny <jenny.kallerup@outlook.com>
2020-01-31 12:53:41 +0100

    Show only campaign products in hero

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using FreakyFashion.Data;
 using FreakyFashion.Data.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace FreakyFashion.Pages
@@ -24,7 +25,11 @@ namespace FreakyFashion.Pages
 
         public void OnGet()
         {
-            ProductList = _context.Products.ToList();
+            ProductList = _context.Products
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .ToList();
+
             CategoryList = _context.Categories.ToList();
             ViewData["CategoryList"] = CategoryList;
         }
