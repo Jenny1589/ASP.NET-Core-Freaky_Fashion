77de558cab018590c4ecb4b5b308f04e52a2cb95
Jenny <jenny.kallerup@outlook.com>
2020-01-31 12:32:14 +0100

    Add category page
    Link to category pages from navbar as well

---
+++ Pages/Category.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashion
+{
+    public class CategoryModel : PageModel
+    {
+        private FreakyFashionContext _context;
+
+        public Category Category { get; private set; }
+        public List<Product> ProductList { get; private set; }
+
+        public CategoryModel(FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet(int id)
+        { 
+            Category = _context.Categories
+                .Include(c => c.ProductCategories)
+                .ThenInclude(pc => pc.Product)
+                .FirstOrDefault(c => c.Id == id);
+
+            ProductList = Category?.ProductCategories
+                .Select(pc => pc.Product)
+                .ToList();
+
+            ViewData["CategoryList"] = _context.Categories.ToList();
+        }
+    }
+}
