0a318a2aa9693aa0550f5d54f4cca6d3938cfa39
Jenny <jenny.kallerup@outlook.com>
2020-01-31 09:58:50 +0100

    Add product page

---
+++ Pages/Product.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+namespace FreakyFashion
+{
+    public class ProductModel : PageModel
+    {
+        private FreakyFashionContext _context;
+        public Product Product { get; private set; }
+
+        public ProductModel(FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet(int id)
+        {
+            Product = _context.Products.Find(id);
+            ViewData["CategoryList"] = _context.Categories.ToList();
+        }
+    }
+}
