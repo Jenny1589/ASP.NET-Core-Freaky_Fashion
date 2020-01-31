8f98c79f2aa686789b4c06c46e24186d8905a7f0
Jenny <jenny.kallerup@outlook.com>
2020-01-31 10:45:13 +0100

    Add article number to database

--- Data/Entities/Product.cs
+++ Data/Entities/Product.cs
@@ -5,21 +5,23 @@ namespace FreakyFashion.Data.Entities
     public class Product
     {
         public int Id { get; protected set; }
+        public string ArticleNumber { get; protected set; }
         public string Name { get; protected set; }
         public string Description { get; protected set; }
         public double Price { get; protected set; }
         public Uri ImageUri { get; protected set; }
 
-        public Product(string name, string description, double price, Uri imageUri)
+        public Product(string articleNumber, string name, string description, double price, Uri imageUri)
         {
+            ArticleNumber = articleNumber;
             Name = name;
             Description = description;
             Price = price;
             ImageUri = imageUri;
         }
 
-        public Product(int id, string name, string description, double price, Uri imageUri)
-            : this(name, description, price, imageUri)
+        public Product(int id, string articleNumber, string name, string description, double price, Uri imageUri)
+            : this(articleNumber, name, description, price, imageUri)
         {
             Id = id;
         }
--- Data/EntityConfigurations/ProductConfiguration.cs
+++ Data/EntityConfigurations/ProductConfiguration.cs
@@ -24,6 +24,7 @@ namespace FreakyFashion.Data.EntityConfigurations
 
             builder.HasData(new Product[] {
                 new Product(id: 1,
+                            articleNumber: "12345-6789",
                             name: "Crazy skirt",
                             description: "Do you want to be the one to stand out in a crowd? " +
                                          "This skirt will fit for any occasion",
@@ -32,6 +33,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 2,
+                            articleNumber: "98765-4321",
                             name: "Wild pants",
                             description: "These pants will certainly get you the attention you deserve. " +
                                          "Be brave, be free, be wild",
@@ -40,6 +42,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 3,
+                            articleNumber: "21543-9876",
                             name: "Furry coatee",
                             description: "This jacket will keep you both warm and dazzling. " +
                                          "It comes in many vibrant colours. What is your furry?",
@@ -48,6 +51,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 4,
+                            articleNumber: "32145-8769",
                             name: "Gorgeous blouse",
                             description: "You will be the center of all attention, 100% guarantee!" +
                                          "Gorgeous will bring out your freaky. Get ready to party!",
@@ -56,6 +60,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 5,
+                            articleNumber: "42137-2897",
                             name: "Green flower dress",
                             description: "This is deffinately THE dress." +
                                          "You will see shapes you didn't know you had.",
@@ -64,6 +69,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 6,
+                            articleNumber: "67921-5690",
                             name: "Cool jeans",
                             description: "Too cool for school? Get theese jeans today!",
                             price: 699,
@@ -71,6 +77,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                                               uriKind: UriKind.Relative)),
 
                 new Product(id: 7,
+                            articleNumber: "75641-0876",
                             name: "Bad girl shirt",
                             description: "You feel bad? No you are just freaky!" +
                                          "Show the real you.",
@@ -78,6 +85,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                             imageUri: new Uri(uriString: "/img/shirt_01.jpg",
                                               uriKind: UriKind.Relative)),
                 new Product(id: 8,
+                            articleNumber: "29064-8514",
                             name: "Freaky beach dress",
                             description: "Own the beach this summer? Of course you want" +
                                          "What are you waiting for?",
@@ -85,6 +93,7 @@ namespace FreakyFashion.Data.EntityConfigurations
                             imageUri: new Uri(uriString: "/img/dress_02.jpg",
                                               uriKind: UriKind.Relative)),
                 new Product(id: 9,
+                            articleNumber: "85201-6354",
                             name: "Get dirty jeans",
                             description: "Casual, comfortable, enjoyable!" +
                                          "No need to get stiff in theese pants",
@@ -92,12 +101,14 @@ namespace FreakyFashion.Data.EntityConfigurations
                             imageUri: new Uri(uriString: "/img/jeans_02.jpg",
                                               uriKind: UriKind.Relative)),
                 new Product(id: 10,
+                            articleNumber: "50945-3165",
                             name: "Pepsi Cola shirt",
                             description: "Are you a Pepsi fan? Show it to the world!",
                             price: 99,
                             imageUri: new Uri(uriString: "/img/shirt_02.jpg",
                                               uriKind: UriKind.Relative)),
                 new Product(id: 11,
+                            articleNumber: "20975-8647",
                             name: "Smokin' dress",
                             description: "Elegant, beautiful, yes just gorgeous - Or as we say: Smokin'",
                             price: 1999.99,
