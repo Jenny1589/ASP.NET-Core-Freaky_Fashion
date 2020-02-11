63fd324b8a7c58637b946397729af904b8aefecf
Jenny <jenny.kallerup@outlook.com>
2020-02-11 09:37:53 +0100

    Make url to products contain url slug instead of id

--- Pages/Product.cshtml.cs
+++ Pages/Product.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using FreakyFashion.Data;
 using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FreakyFashion
 {
@@ -17,12 +19,18 @@ namespace FreakyFashion
             _context = context;
         }
 
-        public void OnGet(int id)
+        public async Task<IActionResult> OnGetAsync(string urlSlug)
         {
-            Product = _context.Products.Find(id);
-            ViewData["CategoryList"] = _context.Categories.ToList();
+            Product = _context.Products
+                .FirstOrDefault(p => p.UrlSlug.Equals(urlSlug.ToLower()));
 
+            if (Product == null) return NotFound();
+
+
+            ViewData["CategoryList"] = _context.Categories.ToList();
             RecommendedProductList = _context.Products.Where(p => !p.Equals(Product)).Take(4).ToList();
+
+            return Page();
         }
     }
 }
