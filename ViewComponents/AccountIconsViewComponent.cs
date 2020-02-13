4579dde8dc43a2c1e38f307a814c1400e6c988a1
Jenny <jenny.kallerup@outlook.com>
2020-02-13 18:26:30 +0100

    Toggle icons when signed in

---
+++ ViewComponents/AccountIconsViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreakyFashion.ViewComponents
+{
+    public class AccountIconsViewComponent : ViewComponent
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountIconsViewComponent(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+                return View("SignedIn", user.UserName);
+            }
+
+            return View();
+        }
+    }
+}
