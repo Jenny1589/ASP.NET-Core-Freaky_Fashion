efe57db162eaeaa0d841faa43b6aa32f0e8ab92a
Jenny <jenny.kallerup@outlook.com>
2020-02-13 18:28:06 +0100

    Add category administration section

---
+++ Areas/Admin/Pages/AdminCategory/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+
+namespace FreakyFashion.Areas.Admin.AdminCategory
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FreakyFashion.Data.FreakyFashionContext _context;
+
+        public DeleteModel(FreakyFashion.Data.FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Categories.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Areas/Admin/Pages/AdminCategory/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+
+namespace FreakyFashion.Areas.Admin.AdminCategory
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FreakyFashion.Data.FreakyFashionContext _context;
+
+        public IndexModel(FreakyFashion.Data.FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Categories.ToListAsync();
+        }
+    }
+}
