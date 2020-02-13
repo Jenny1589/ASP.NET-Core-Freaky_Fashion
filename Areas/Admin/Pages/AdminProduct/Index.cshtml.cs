33da2ccd38428248bb6c9f76f67155ba02e58e8d
Jenny <jenny.kallerup@outlook.com>
2020-02-13 18:27:50 +0100

    Add product administration section

---
+++ Areas/Admin/Pages/AdminProduct/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+
+namespace FreakyFashion.Areas.Admin.AdminProduct
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FreakyFashion.Data.FreakyFashionContext _context;
+
+        public IndexModel(FreakyFashion.Data.FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
