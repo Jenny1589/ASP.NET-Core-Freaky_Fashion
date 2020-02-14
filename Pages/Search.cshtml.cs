cea5eff7c65d00e300f641803d043664270966c8
Jenny <jenny.kallerup@outlook.com>
2020-02-14 20:11:01 +0100

    Implement search function

---
+++ Pages/Search.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+
+namespace FreakyFashion
+{
+    public class SearchModel : PageModel
+    {
+        private readonly FreakyFashionContext _context;
+
+        public string Query { get; private set; }
+
+        public SearchModel(FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> SearchResult { get;set; }
+
+        public async Task OnGetAsync(string query)
+        {
+            Query = query;
+
+            SearchResult = await _context.Products
+                .Where(p => p.Name.ToUpper().Contains(query.ToUpper()))
+                .ToListAsync();
+        }
+    }
+}
