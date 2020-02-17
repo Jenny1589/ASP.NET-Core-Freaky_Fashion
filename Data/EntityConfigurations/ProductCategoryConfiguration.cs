88dcd0ac7bee15433be5d119d8819466f1762023
Jenny <jenny.kallerup@outlook.com>
2020-02-17 15:20:00 +0100

    Remove data seed from ProductCategory configuration

--- Data/EntityConfigurations/ProductCategoryConfiguration.cs
+++ Data/EntityConfigurations/ProductCategoryConfiguration.cs
@@ -18,33 +18,6 @@ namespace FreakyFashion.Data.EntityConfigurations
             builder.HasOne(pc => pc.Category)
                 .WithMany(c => c.ProductCategories)
                 .HasForeignKey(pc => pc.CategoryId);
-
-            builder.HasData(new ProductCategory[]
-            {
-                new ProductCategory(1, 1),
-                new ProductCategory(1, 3),
-                new ProductCategory(1, 6),
-                new ProductCategory(2, 2),
-                new ProductCategory(2, 6),
-                new ProductCategory(3, 2),
-                new ProductCategory(3, 5),
-                new ProductCategory(4, 1),
-                new ProductCategory(4, 5),
-                new ProductCategory(5, 3),
-                new ProductCategory(5, 7),
-                new ProductCategory(6, 6),
-                new ProductCategory(7, 3),
-                new ProductCategory(7, 4),
-                new ProductCategory(8, 1),
-                new ProductCategory(8, 7),
-                new ProductCategory(9, 2),
-                new ProductCategory(9, 6),
-                new ProductCategory(10, 2),
-                new ProductCategory(10, 4),
-                new ProductCategory(11, 2),
-                new ProductCategory(11, 3),
-                new ProductCategory(11, 7),
-            });
         }
     }
 }
