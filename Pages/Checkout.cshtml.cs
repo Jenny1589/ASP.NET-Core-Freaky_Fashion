707fda59d8cd7f2cfcf3aabdb775f161f581e107
Jenny <jenny.kallerup@outlook.com>
2020-02-17 15:02:24 +0100

    Create checkout page

---
+++ Pages/Checkout.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace FreakyFashion
+{
+    public class CheckoutModel : PageModel
+    {
+        public Cart Cart { get; private set; }
+
+        public void OnGet()
+        {
+            Cart = JsonConvert.DeserializeObject<Cart>(HttpContext.Session.GetString("Cart"));
+        }
+    }
+}
