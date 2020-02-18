36f6c592fbcca3c628de3f8919ce0ef4a5190c5d
Jenny <jenny.kallerup@outlook.com>
2020-02-18 11:16:09 +0100

    Change possition of sign out function

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using FreakyFashion.Data.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +11,10 @@ namespace FreakyFashion.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<FreakyFashionUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<FreakyFashionUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
---
+++ ViewComponents/SignOutViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreakyFashion.ViewComponents
+{
+    public class SignOutViewComponent : ViewComponent
+    {
+
+        private readonly SignInManager<FreakyFashionUser> _signInManager;
+
+        public SignOutViewComponent(SignInManager<FreakyFashionUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (_signInManager.IsSignedIn(HttpContext.User))
+            {
+                return View();
+            }
+
+            return Content(string.Empty);
+        }
+    }
+}
