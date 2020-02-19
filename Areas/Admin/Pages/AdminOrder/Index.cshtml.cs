88cf9f57de53350518a04e576ddc5fd3ba2af1f5
Jenny <jenny.kallerup@outlook.com>
2020-02-19 16:43:33 +0100

    Add Admin area for orders

---
+++ Areas/Admin/Pages/AdminOrder/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FreakyFashion.Data.Entities;
+
+namespace FreakyFashion
+{
+    public class IndexModel : PageModel
+    {
+        private readonly FreakyFashion.Data.FreakyFashionContext _context;
+
+        public IndexModel(FreakyFashion.Data.FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Order> OrderList { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            OrderList = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.OrderContent)
+                .ThenInclude(oi => oi.Product)
+                .ToListAsync();
+        }
+    }
+}
