563b2e520fa1455f765d84c43f85d0a84fcfa94d
Jenny <jenny.kallerup@outlook.com>
2020-03-02 13:59:30 +0100

    Add api controller for categories

---
+++ Areas/API/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreakyFashion.Data;
+using FreakyFashion.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FreakyFashion.Areas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly FreakyFashionContext _context;
+
+        public ProductController(FreakyFashionContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
